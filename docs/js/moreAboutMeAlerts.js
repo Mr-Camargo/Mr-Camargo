8c411a8c8c381818dd62c4aca7e41638b1b0a14f
Sebastián Camargo <camargo@tutamail.com>
2021-12-09 14:49:55 -0500

    Improved 'More about me' page
    This commit adds mobile support for 'more.html', and also adds some basic Javascript to create alerts when the user clicks a fun fact.

---
+++ docs/js/moreAboutMeAlerts.js
@@ -0,0 +1,22 @@
+console.log('Script loaded!');
+
+
+function koreanAlertBox() {
+    alert("It's Korean for a casual greeting");
+    return;
+};
+
+function airpodsFact() {
+    alert("(and an Apple Pencil on the yard of my school)");
+    return;
+};
+
+function ripCarry() {
+    alert("Carry the cart\n2018-2020(?)");
+    return;
+};
+
+function areYouLost() {
+    alert('"Are you lost?"\n-Disney Worker, December 9th, 2020');
+    return;
+};
